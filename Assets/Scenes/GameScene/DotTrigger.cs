07b28997f2f3760c7dd2474450e370b816060720
Nikita Kharseko <nikita.harseko@itexus.com>
2020-09-24 15:17:15 +0300

    initial commit

---
+++ Assets/Scenes/GameScene/DotTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        var enemy: GameObject = GameObject.Find("dot");
+        if (collider.gameObject.name == "player") {
+            SpawnCube();
+        }
+    }
+}
---
+++ Assets/Scenes/GameScene/GameScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+    public GameObject dotA;
+    public GameObject dotB;
+    public GameObject player;
+    
+    private float speed = 1.5f;
+    private GameObject selectedDot;
+
+    // Start is called before the first frame update
+    void Start() {
+        selectedDot = dotA;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        foreach (Touch touch in Input.touches) {
+            if(touch.phase == TouchPhase.Began) Debug.Log("123");
+        }
+        movePlayerToDots();
+    }
+
+    private void movePlayerToDots() {
+        var dotPosition = selectedDot.transform.position; 
+        var playerPosition = player.transform.position; 
+        player.transform.position = Vector3.MoveTowards (playerPosition, dotPosition, Time.deltaTime * speed); 
+        
+        if (playerPosition == dotPosition) {
+            changeSelectedDot();
+        }
+    }
+
+    private void changeSelectedDot() {
+        if (selectedDot == dotA) {
+            selectedDot = dotB;
+        } else if (selectedDot == dotB) {
+            selectedDot = dotA;
+        }
+    }
+}
